1664c30d83b14e10e12b0f72ba2769f7d13db4da
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2022-01-13 17:56:12 +0100

    Setup Typescript, EsLint and Prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,30 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "../lib/posts";
-import Link from "next/link";
-import Date from "../components/date";
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
+import { GetStaticProps } from 'next';
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = () => {
   const allPosts = getSortedPostsData();
 
   return {
     props: {
-      allPosts,
-    },
+      allPosts
+    }
   };
-}
+};
 
-export default function Home({ allPosts }) {
+export default function Home({
+  allPosts
+}: {
+  allPosts: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) {
   return (
     <Layout home>
       <Head>
@@ -24,7 +33,7 @@ export default function Home({ allPosts }) {
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
+          (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
